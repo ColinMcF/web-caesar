40e49cedefbc2319785bece5d63bb5d05b2e7891
Colin McFarquhar <colin2mc@gmail.com>
2017-12-21 18:41:10 -0500

    added files for submission

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+        <form action="/" method="post">
+            rotate by: <input type="text" id="rot" name="rot" value="0"><br>
+            <textarea id="text" name="text" >{0}</textarea>
+            <input type="submit" value="submit">
+        </form>
+        <br>
+        
+
+    </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form.format("")
+
+@app.route("/" ,methods= ['POST'])
+def encrypt():
+    encrypt_text=(request.form["text"])
+    encrypt_rot=int(request.form["rot"])
+
+    final_text=rotate_string(encrypt_text,encrypt_rot)
+    return form.format(final_text)
 
 app.run()
